74b2ffeed8fb8ce01ed0e7d73d46ee9caa17f116
Matthew Schneider <purpleslurple@gmail.com>
2023-08-12 11:54:06 -0500

    Update ps-hyperscope.php
    Add note regarding ChatGPT code generation.

--- ps-hyperscope.php
+++ ps-hyperscope.php
@@ -1,4 +1,5 @@
 <?php
+// This code was generated using ChatGPT, in response to prompts by Matthew A. Schneider. See: https://chat.openai.com/share/eb6683ba-6540-433f-a598-03bc57b13f4f
 
 $DISCLAIMER = "<p>This page has been modified by a program for demonstration purposes.</p>";
 
